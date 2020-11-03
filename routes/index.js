53124d7a48ece765639a586cdef5d7f0dcdc8bc7
tanhank2k <tanhank2k@gmail.com>
2020-11-03 17:12:03 +0700

    fix radio

--- routes/index.js
+++ routes/index.js
@@ -14,31 +14,31 @@ router.post('/',function(req,res) {
   const num2= req.body.snum;
   var result = 0;
 
-  var multiNode = req.body.multi;
-  if(req.body.multi=='on') {
+  var multiNode = req.body.calc;
+  if(req.body.calc=='1') {
     multiNode = 'checked';
     result = Number(num1) * Number(num2);
   };
 
-  var divideNode = req.body.divide;
-  if(req.body.divide=='on') {
+  var divideNode = req.body.calc;
+  if(req.body.calc=='2') {
     divideNode = 'checked';
     result = Number(num1) / Number(num2);};
 
-  var plusNode = req.body.plus;
-  if(req.body.plus=='on') {
+  var plusNode = req.body.calc;
+  if(req.body.calc=='3') {
     plusNode = 'checked';
     result = Number(num1) + Number(num2);};
 
-  var subNode = req.body.sub;
-  if(req.body.sub=='on') {
+  var subNode = req.body.calc;
+  if(req.body.calc=='4') {
     subNode = 'checked';
     result = Number(num1) - Number(num2);};
 
 
   console.log(req.body);
   console.log("Post");
-  res.render('index',{title: 'Bé Tập Tính',number1:num1, number2: num2,check_multi:multiNode,check_divide:divideNode,
-  check_plus:plusNode,check_sub:subNode, result_value: result});
+  res.render('index',{title: 'Bé Tập Tính',number1:num1, number2: num2,
+  check_multi: multiNode, check_divide: divideNode,check_plus: plusNode,check_sub: subNode, result_value: result});
 })
 module.exports = router;
